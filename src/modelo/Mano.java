d567d57faa60f0c49dff973f02a8249336d0960b
carrony <davidecasas@gmail.com>
2024-05-27 11:37:14 +0200

    Clase mano modificada
    modificado el constructor y añadido el metodo valorMano que devuelve la
    puntuacion de la mano. Falta implementar el caso del AS que pueda valer
    1 u 11

--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -1,9 +1,19 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Mano extends Mazo {
 
 	public Mano() {
+		//ceramos una list de cartas vacía
+		this.cartas = new ArrayList<Carta>();
 	}
 
-	
+	public int valorMano() {
+		int valor=0;
+		for(Carta c : this.cartas) {
+			valor = valor + c.getValor();
+		}
+		return valor;
+	}
 }
