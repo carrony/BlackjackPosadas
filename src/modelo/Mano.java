a5813eb391f22cd60b8e21c3764f76d1e840a144
carrony <davidecasas@gmail.com>
2024-05-28 09:17:47 +0200

    Clase Mano modificada
    Modificado el método valorMano para que los ases puedan valer tanto 1
    como 11, añadido el método finDeJuego y toString

--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -11,9 +11,38 @@ public class Mano extends Mazo {
 
 	public int valorMano() {
 		int valor=0;
+		int numAses=0;
 		for(Carta c : this.cartas) {
+			if (c.getNumero()==1) {
+				numAses++;
+			}
 			valor = valor + c.getValor();
 		}
+		// comprobamos si me he pasado de 21 
+		while (numAses>0 && valor>21) {
+			valor=valor-10;
+			numAses--;
+		}
+	
+		
 		return valor;
 	}
+	
+	 // Version del método que lanza la excepción Masde21Exception
+	public boolean finDeJuego() {
+		if (valorMano()>=21) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Mano [" + cartas + "] Valor de la mano:"+valorMano();
+	}
+
+
+	
+	
+	
 }
---
+++ src/modelo/Masde21Exception.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public class Masde21Exception extends Exception {
+
+}
